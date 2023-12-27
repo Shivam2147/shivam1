de8202797f5f04359b20902d46d672e3aa7c7a1f
shivam <sharmashivam52465@gmail.com>
2023-12-27 13:42:32 +0530

    all commit

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
 
-function init() {
-    let menu = document.getElementById("menu");
-    let nav = document.getElementById("mab_nav")
-    let smallpen = false;
+// function init() {
+//     let menu = document.getElementById("menu")
+//     let nav = document.getElementById("mab_nav")
+//     let smallpen = false;
 
-    document.addEventListener('DOMContentLoaded', () => {
-        if (smallpen === false) {
-            nav.style.right = "-100%"
-        }
-    })
-    menu.addEventListener('click', () => {
-        if (smallpen != true) {
-            nav.style.right = '0';
-            smallpen = true;
-        }
-    })
+//     document.addEventListener('DOMContentLoaded', () => {
+//         if (smallpen === false) {
+//             nav.style.right = "-100%"
+//         }
+//     })
+//     menu.addEventListener('click', () => {
+//         if (smallpen != true) {
+//             nav.style.right = '0';
+//             smallpen = false;
+//         }
+//     })
 
-    nav.children[0].addEventListener('click', () => {
-        nav.style.right = '-100%';
-        smallpen = false;
-    })
+//     nav.children[0].addEventListener('click', () => {
+//         nav.style.right = '-100%';
+//         smallpen = true;
+//     })
+// }
 
-}
+let sidebar_open = document.getElementById('mab_nav')
+let sidebar_close = document.getElementById('menu')
+let close1 = document.getElementById('close')
+let small_open = false
 
+sidebar_close.addEventListener('click', () =>{
+    sidebar_open.style.right = '0%'
+})
 
+close1.addEventListener('click', () =>{
+    sidebar_open.style.right = '-100%'
+    // console.log('hello');
+})
 
 
 
